d3a3602443653dc81233986f9ae78a62c09064bf
Barrette Banner <barrettebanner@gmail.com>
2019-07-12 16:13:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,14 @@ def roll_call_dwarves(array)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  new_array = []
+  array.each do |word| 
+    new_array.push("#{word.capitalize}!")
+    
+  end
+  return new_array
 end
 
 def long_planeteer_calls# code an argument here
